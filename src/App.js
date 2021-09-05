dd45c663528cdf71fee99522b48f14be4b1b4530
Katarka <www.runit@gmail.com>
2021-09-05 21:16:56 +0300

    Add grid

--- src/App.js
+++ src/App.js
@@ -4,7 +4,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-
+      <body>
+      <div class="grid-container">
+        <div class="item1">1</div>
+        <div class="item2">2</div>
+        <div class="item3">3</div>
+        <div class="item4">4</div>
+        <div class="item5">5</div>
+      </div>
+      </body>
     </div>
   );
 }
