10e4ba6212f5ec6ea5ebcb43c6759167dc1df202
Katarka <www.runit@gmail.com>
2022-04-14 15:08:44 +0300

    refactoring

---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import style from "./navbar.module.css"
+import {NavLink} from "react-router-dom";
+
+const navbar = () => {
+    return (
+        <nav className={style.nav}>
+            <div className={style.item}>
+                <div>
+                    <NavLink to='/classic' activeClassName={style.active}>Classic</NavLink>
+                </div>
+                <div>
+                    <NavLink to='/bk' activeClassName={style.active}>Burning Crusader</NavLink>
+                </div>
+                <div>
+                    <NavLink to='/lk' activeClassName={style.active}>Lich King</NavLink>
+                </div>
+                <div>
+                    <NavLink to='/pand' activeClassName={style.active}>Pandaria</NavLink>
+                </div>
+                <div>
+                    <NavLink to='/drenour' activeClassName={style.active}>Drenour</NavLink>
+                </div>
+                <div>
+                    <NavLink to='/legion' activeClassName={style.active}>Legion</NavLink>
+                </div>
+                <div>
+                    <NavLink to='/bfa' activeClassName={style.active}>BFA</NavLink>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default navbar
