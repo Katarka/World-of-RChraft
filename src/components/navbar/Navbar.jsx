bb590bfa1507b9a283498ffd340f8fe5c887f5a7
Katarka <www.runit@gmail.com>
2022-06-01 17:41:02 +0300

    css refactoring flex

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -6,25 +6,39 @@ const navbar = () => {
     return (
         <nav className={style.nav}>
                 <div className={style.item}>
-                    <NavLink to='/classic' activeClassName={style.active}>Classic</NavLink>
+                    <NavLink to='/classic' activeClassName={style.active}>
+                        <button className={style.button}>Classic</button>
+                    </NavLink>
                 </div>
                 <div className={style.item}>
-                    <NavLink to='/bc' activeClassName={style.active}>Burning Crusader</NavLink>
+                    <NavLink to='/bc' activeClassName={style.active}>
+                        <button className={style.button}>Burning Crusader</button>
+                    </NavLink>
                 </div>
                 <div className={style.item}>
-                    <NavLink to='/wotlk' activeClassName={style.active}>Lich King</NavLink>
+                    <NavLink to='/wotlk' activeClassName={style.active}>
+                        <button className={style.button}>Lich King</button>
+                    </NavLink>
                 </div>
                 <div className={style.item}>
-                    <NavLink to='/pand' activeClassName={style.active}>Pandaria</NavLink>
+                    <NavLink to='/pand' activeClassName={style.active}>
+                        <button className={style.button}>Pandaria</button>
+                        </NavLink>
                 </div>
                 <div className={style.item}>
-                    <NavLink to='/drenour' activeClassName={style.active}>Drenour</NavLink>
+                    <NavLink to='/drenour' activeClassName={style.active}>
+                        <button className={style.button}>Drenour</button>
+                    </NavLink>
                 </div>
                 <div className={style.item}>
-                    <NavLink to='/legion' activeClassName={style.active}>Legion</NavLink>
+                    <NavLink to='/legion' activeClassName={style.active}>
+                        <button className={style.button}>Legion</button>
+                        </NavLink>
                 </div>
                 <div className={style.item}>
-                    <NavLink to='/bfa' activeClassName={style.active}>BFA</NavLink>
+                    <NavLink to='/bfa' activeClassName={style.active}>
+                        <button className={style.button}>BFA</button>
+                    </NavLink>
                 </div>
         </nav>
     )
