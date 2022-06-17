56866220d9dfa17c23f4fcc5a57d54386fefc9f6
Katarka <www.runit@gmail.com>
2022-06-17 13:06:17 +0300

    leftbar custom

--- src/components/Leftbar/Leftbar.jsx
+++ src/components/Leftbar/Leftbar.jsx
@@ -1,65 +1,48 @@
 import React from "react";
 import cn from "classnames";
 import { NavLink } from "react-router-dom";
-
 import style from "./Leftbar.module.scss"
 
 const Leftbar = () => {
     return (
         <nav className={style.nav}>
-            <NavLink 
-                to='/classic'
-                className={({isActive}) => (!isActive ? style.item : cn(style.item + ' ' + style.active))}
-            >   
+            <NavLink to='/classic'
+                className={({isActive}) => (!isActive ? style.item : cn(style.item + ' ' + style.active))}>
                 Classic
             </NavLink>
 
-            <NavLink 
-                to='/bc'
-                className={({isActive}) => (!isActive ? style.item : cn(style.item + ' ' + style.active))}
-            >
+            <NavLink to='/bc'
+                className={({isActive}) => (!isActive ? style.item : cn(style.item + ' ' + style.active))}>
                 The Burning Crusade
             </NavLink>
 
-            <NavLink 
-                to='/wotlk' 
-                className={({isActive}) => (!isActive ? style.item : cn(style.item + ' ' + style.active))}
-            >
+            <NavLink to='/wotlk'
+                className={({isActive}) => (!isActive ? style.item : cn(style.item + ' ' + style.active))}>
                 Wrath of the Lich King
             </NavLink>
 
-            <NavLink 
-                to='/pand' 
-                className={({isActive}) => (!isActive ? style.item : cn(style.item + ' ' + style.active))}
-            >
+            <NavLink to='/pand'
+                className={({isActive}) => (!isActive ? style.item : cn(style.item + ' ' + style.active))}>
                 Mists of Pandaria
             </NavLink>
 
-            <NavLink 
-                to='/drenour' 
-                className={({isActive}) => (!isActive ? style.item : cn(style.item + ' ' + style.active))}
-            >
+            <NavLink to='/drenour'
+                className={({isActive}) => (!isActive ? style.item : cn(style.item + ' ' + style.active))}>
                 Warlords of Draenor
             </NavLink>
 
-            <NavLink 
-                to='/legion' 
-                className={({isActive}) => (!isActive ? style.item : cn(style.item + ' ' + style.active))}
-            >
+            <NavLink to='/legion'
+                className={({isActive}) => (!isActive ? style.item : cn(style.item + ' ' + style.active))}>
                 Legion
             </NavLink>
 
-            <NavLink 
-                to='/bfa' 
-                className={({isActive}) => (!isActive ? style.item : cn(style.item + ' ' + style.active))}
-            >
+            <NavLink to='/bfa'
+                className={({isActive}) => (!isActive ? style.item : cn(style.item + ' ' + style.active))}>
                 Battle for Azeroth
             </NavLink>
 
-            <NavLink 
-                to='/shadowlands' 
-                className={({isActive}) => (!isActive ? style.item : cn(style.item + ' ' + style.active))}
-            >
+            <NavLink to='/shadowlands'
+                className={({isActive}) => (!isActive ? style.item : cn(style.item + ' ' + style.active))}>
                 Shadowlands
             </NavLink>
         </nav>
--- src/function/random.js
+++ src/function/random.js
@@ -1,4 +1,3 @@
 const getRandomValueFromArray = arr => arr[Math.floor(Math.random() * arr.length)]
 
-
 export default getRandomValueFromArray
