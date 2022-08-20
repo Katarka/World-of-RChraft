ae96783f92de47b19282141904313fa32b5b49de
gmx <www.runit@gmail.com>
2022-08-20 18:32:32 +0300

    refactor

--- src/containers/RandomizerPage/CataPage/Cata.jsx
+++ src/containers/RandomizerPage/CataPage/Cata.jsx
@@ -3,11 +3,11 @@ import style from '../../../styles/stylesRandomize.module.css';
 import Leftbar from '../../../components/RandomizerPage/NavigationRandom/NavigationRandom';
 import elf from '../../../assets/svg/elf.svg';
 import {useDispatch, useSelector} from "react-redux";
-import {nextRandomCata} from "../../../toolkitRedux/toolkitReducer";
+import {nextRandomCataclysm} from "../../../toolkitRedux/toolkitReducer";
 
 
-const Cata = () => {
-    const newRandomCata = useSelector(state => state.toolkit.newRandomCata)
+const Cataclysm = () => {
+    const newRandomCataclysm = useSelector(state => state.toolkit.newRandomCata)
     const dispatch = useDispatch()
     return (
         <>
@@ -16,9 +16,9 @@ const Cata = () => {
                 <div className={style.content}>
                     <div className={style.backText}>
                         <div>
-                            <h1 className={style.h1}>{newRandomCata.join('\n')}</h1>
+                            <h1 className={style.h1}>{newRandomCataclysm.join('\n')}</h1>
                         </div>
-                        <button className={style.button} onClick={() => dispatch(nextRandomCata())}>Go
+                        <button className={style.button} onClick={() => dispatch(nextRandomCataclysm())}>Go
                         </button>
                     </div>
                     <img className={style.img} src={elf} alt='#'/>
@@ -32,4 +32,4 @@ const Cata = () => {
     )
 }
 
-export default Cata
+export default Cataclysm
