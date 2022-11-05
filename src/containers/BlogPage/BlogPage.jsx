bec3bcba04e1f329cecd6e6c9f6b9864dd8561cc
EugenyGorshkov <gorshkov.zhenya@mail.ru>
2022-11-05 15:29:29 +0300

    add time format to blogPage

--- src/containers/BlogPage/BlogPage.jsx
+++ src/containers/BlogPage/BlogPage.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import { useQuery } from "@apollo/client";
 import { DocumentRenderer } from '@keystone-6/document-renderer';
 
@@ -10,7 +10,32 @@ import bgimage from '../../assets/backgroundS.jpg'
 const BlogPage = () => {
 
     const { loading, error, data } = useQuery(FETCH_PAGINATED_POSTS)
-    console.log(data);
+    // console.log(data);
+
+    let parceFunc = (el) => {
+        let date = new Date(el);
+
+        const dateOptions = {
+            day: 'numeric',
+            month: 'long',
+            year: '2-digit',
+        };
+        const timeOptions = {
+            hour: '2-digit',
+            minute: '2-digit',
+        };
+
+        const dateFormatRU = new Intl.DateTimeFormat('ru', dateOptions);
+        const timeFormatRU = new Intl.DateTimeFormat('ru', timeOptions);
+
+        return (
+            <>            
+                <span className="whitespace-nowrap">{`${dateFormatRU.format(date)}`}</span><br />
+                <span className="whitespace-nowrap">{`${timeFormatRU.format(date)}`}</span>
+            </>
+
+        )
+    }
 
     if (loading) {
         return <h2 className={styles.loader}>Loading...</h2>
@@ -18,9 +43,9 @@ const BlogPage = () => {
 
     if (error) {
         return (
-        <div style={{backgroundImage: `url(${bgimage})`}} className='w-full h-full object-cover object-center'>
-            <h2 className='text-center pt-12 text-white'>Could not fetch. We are working at problem</h2>
-        </div>
+            <div style={{ backgroundImage: `url(${bgimage})` }} className='w-full h-full object-cover object-center'>
+                <h2 className='text-center pt-12 text-white'>Could not fetch. We are working at problem</h2>
+            </div>
         )
     }
 
@@ -42,27 +67,26 @@ const BlogPage = () => {
         //         </div>
         //     </div>
         // </>
-                <div className={styles.blogPage}>
-                    <div className={styles.wrapper}>
-                    <h1 className={styles.BlogMainTitle}>Блог великолепной, Шонни!</h1>
-                        {data.posts.map(el => {
-                            console.log(el)
-                            return (
-                                    <div className={styles.blogItemWrapper} key={el.title} id={el.id}>
-                                        <div className={styles.blogItemTopRow}>
-                                            <h2 className={styles.blogItemTitle}>{el.title}</h2>
-                                            <div className={styles.blogItemDate}>
-                                                <span className={styles.dateNum}>{el.publishedAt}</span>
-                                                <span className={styles.dateMon}>dec</span>
-                                            </div>
-                                        </div>
-                                        <DocumentRenderer document={el.content.document} />
-                                    </div>
-                            )
-                        }
-                        )}
-                    </div>
-                </div>
+        <div className={styles.blogPage}>
+            <div className={styles.wrapper}>
+                <h1 className={styles.BlogMainTitle}>Блог великолепной, Шонни!</h1>
+                {data.posts.map(el => {
+                    console.log(el)
+                    return (
+                        <div className={styles.blogItemWrapper} key={el.title} id={el.id}>
+                            <div className={styles.blogItemTopRow}>
+                                <h2 className={styles.blogItemTitle}>{el.title}</h2>
+                                <div className={styles.blogItemDate}>
+                                    <span className={styles.dateNum}>{parceFunc(el.publishedAt)}</span>
+                                </div>
+                            </div>
+                            <DocumentRenderer document={el.content.document} />
+                        </div>
+                    )
+                }
+                )}
+            </div>
+        </div>
     )
 }
 
