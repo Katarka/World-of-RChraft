92f7500de3ae08e07147327a5feef3f7741f3e0d
gmx <www.runit@gmail.com>
2022-11-08 21:46:30 +0300

    add new homePage

--- src/components/HomePage/Slider/Slider.jsx
+++ src/components/HomePage/Slider/Slider.jsx
@@ -1,23 +1,21 @@
 import React from "react";
-import classic from "../../../assets/img/classic.jpg"
-import bc from "../../../assets/img/bc.jpg"
-import lc from "../../../assets/img/lc.jpg"
 
 
 const Slider = () => {
     return (
-        <div className="h-screen w-auto bg-blue-800">
-            <div class="flex items-baseline justify-center pt-24">
-                <div class="pt-28 m-6 h-[530px] w-24 bg-gray-900">01</div>
-                <div class="pt-14 m-6 h-[530px] w-24 bg-gray-900">02</div>
-                <div class="pt-0 m-6 h-[530px] w-24 bg-gray-900">03</div>
-                <div class="pt-14 m-6 h-[530px] w-24 bg-gray-900">02</div>
-                <div class="pt-28 m-6 h-[530px] w-24 bg-gray-900">01</div>
-                <div class="pt-14 m-6 h-[530px] w-24 bg-gray-900">02</div>
-                <div class="pt-0 m-6 h-[530px] w-24 bg-gray-900">03</div>
-                <div class="pt-14 m-6 h-[530px] w-24 bg-gray-900">02</div>
-                <div class="pt-28 m-6 h-[530px] w-24 bg-gray-900">01</div>
-                <div class="pt-14 m-6 h-[530px] w-24 bg-gray-900">02</div>
+        <div className="h-screen w-auto bg-homeBg">
+            <div className="h-screen flex items-baseline justify-center pt-24 pb-24 bg-gradient-to-r from-blue-700">
+                <div className="pt-28 m-6 h-128 w-24 bg-slice1 will-change-auto hover:will-change-scroll">.</div>
+                {/*512px & 96px*/}
+                <div className="pt-14 m-6 h-128 w-24 bg-slice2 ">.</div>
+                <div className="pt-0 m-6 h-128 w-24 bg-slice3">.</div>
+                <div className="pt-14 m-6 h-128 w-24 bg-slice4">.</div>
+                <div className="pt-28 m-6 h-128 w-24 bg-slice5">.</div>
+                <div className="pt-14 m-6 h-128 w-24 bg-slice6">.</div>
+                <div className="pt-0 m-6 h-128 w-24 bg-slice7">.</div>
+                <div className="pt-14 m-6 h-128 w-24 bg-slice8">.</div>
+                <div className="pt-28 m-6 h-128 w-24 bg-slice9">.</div>
+                <div className="pt-14 m-6 h-128 w-24 bg-slice10">.</div>
             </div>
         </div>
     )
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,23 @@ module.exports = {
       '2xl': '1400px',
     },
     extend: {
+      height: {
+        '128': '32rem',
+      },
+      backgroundImage: {
+        'slice1': "url('/src/assets/Slider/bcSlice1.png')",
+        'slice2': "url('/src/assets/Slider/bcSlice2.png')",
+        'slice3': "url('/src/assets/Slider/bcSlice3.png')",
+        'slice4': "url('/src/assets/Slider/bcSlice4.png')",
+        'slice5': "url('/src/assets/Slider/bcSlice5.png')",
+        'slice6': "url('/src/assets/Slider/bcSlice6.png')",
+        'slice7': "url('/src/assets/Slider/bcSlice7.png')",
+        'slice8': "url('/src/assets/Slider/bcSlice8.png')",
+        'slice9': "url('/src/assets/Slider/bcSlice9.png')",
+        'slice10': "url('/src/assets/Slider/bcSlice10.png')",
+        'homeBg': "url('/src/assets/homeBg.png')",
+        'footer-texture': "url('/img/footer-texture.png')",
+      }
 },
   },
   plugins: [
